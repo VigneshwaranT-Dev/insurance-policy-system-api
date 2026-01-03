9dc83302ce9971458206f76eaf1610c7eef05702
Vigneshwaran T <vigneshwarant54@gmail.com>
2026-01-03 22:11:16 +0530

    chore: connect mongoDB and add dummy user

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
+const { User } = require("./models");
 
 const PORT = process.env.PORT || 5000;
+
+connectDB(); //connect Database
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+(async () => {
+  const user = await User.create({
+    name: "Vigneshwaran Thiruselvam",
+    email: "vthiruselvam@gmail.com",
+    password: "Vignesh@2026",
+  });
+
+  console.log(user);
+})();
